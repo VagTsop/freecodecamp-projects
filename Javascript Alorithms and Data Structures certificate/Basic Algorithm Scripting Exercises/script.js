8cb15f668b9b5c9b427f5f59c879f399773fab92
VagTsop <vatsop52@gmail.com>
2022-01-01 21:16:52 +0200

    Finders Keepers solution

--- Javascript Alorithms and Data Structures certificate/Basic Algorithm Scripting Exercises/script.js
+++ Javascript Alorithms and Data Structures certificate/Basic Algorithm Scripting Exercises/script.js
@@ -139,3 +139,21 @@ truncateString(
 );
 */
 
+/*
+- Finders Keepers
+reate a function that looks through an array arr and returns the first element 
+in it that passes a 'truth test'.  This means that given an element x, the 'truth test'
+is passed if func(x) is true. If no element passes the test, return undefined.
+
+
+function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      return arr[i];
+    }
+  }
+}
+findElement([1, 3, 5, 8, 9, 10], function (num) {
+  return num % 2 === 0;
+});
+*/
