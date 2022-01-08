5ddf6f9a9d0fadc0e8cfc1cd7b027a5523b46948
VagTsop <vatsop52@gmail.com>
2022-01-08 19:52:01 +0200

    add 'Where do I Belong' solution

--- Javascript Alorithms and Data Structures certificate/Basic Algorithm Scripting Exercises/script.js
+++ Javascript Alorithms and Data Structures certificate/Basic Algorithm Scripting Exercises/script.js
@@ -216,3 +216,20 @@ function bouncer(arr) {
 
 bouncer([7, "ate", "", false, 9]);
 */
+
+/*
+- Where do I Belong
+
+Return the lowest index at which a value (second argument) should be inserted into 
+an array (first argument) once it has been sorted. The returned value should be a number.
+
+function getIndexToIns(arr, num) {
+  arr.push(num)
+  arr.sort((a, b) => {
+    if (a > b) return 1;
+    if (a < b) return -1;
+  });
+  return arr.indexOf(num);
+}
+getIndexToIns([10, 20, 30, 40, 50], 35)
+*/
