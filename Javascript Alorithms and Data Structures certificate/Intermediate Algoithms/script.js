177956270add51cac8f1c3e9c374ebab636c9ae1
VagTsop <vatsop52@gmail.com>
2022-01-09 11:14:37 +0200

    add 'Dif Two Arrays' Solution

--- Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
+++ Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
@@ -22,3 +22,20 @@ function sumAll(arr) {
 }
 console.log(sumAll([4, 1]));
 */
+
+/*
+- Diff Two Arrays
+
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both.
+In other words, return the symmetric difference of the two arrays.
+
+function diffArray(arr1, arr2) {
+  const arr2exist = arr1.filter(item => arr2.indexOf(item) === -1)
+  const arr1exist = arr2.filter(item => arr1.indexOf(item) === -1)
+  const finalArr = [...arr2exist, ...arr1exist]
+  return finalArr;
+}
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]))
+
+
+*/
