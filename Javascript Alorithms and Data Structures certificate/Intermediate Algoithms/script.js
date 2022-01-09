67dd3a8b91240fce2e4049e1a1cfc931c6a9fb5d
VagTsop <vatsop52@gmail.com>
2022-01-09 11:53:11 +0200

    add 'Seek and Destroy Solution'

--- Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
+++ Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
@@ -36,6 +36,20 @@ function diffArray(arr1, arr2) {
   return finalArr;
 }
 console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]))
+*/
 
+/*
+- Seek and Destroy
 
+You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments.
+Remove all elements from the initial array that are of the same value as these arguments.
+
+Note: You have to use the arguments object.
+
+function destroyer(arr) {
+  let newArr = [...arguments];
+  return newArr[0].filter(item => newArr.indexOf(item) === -1);
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 */
