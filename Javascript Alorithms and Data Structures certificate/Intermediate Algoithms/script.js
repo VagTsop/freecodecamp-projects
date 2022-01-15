db94b88f2eb18d90d40aa471c7e0a09b873adcd5
VagTsop <vatsop52@gmail.com>
2022-01-15 17:26:54 +0200

    add 'DNA Pairing exercise'

--- Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
+++ Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
@@ -194,3 +194,61 @@ function myReplace(str, before, after) {
 myReplace("I think we should look up there", "up", "Down");
 
 */
+
+/*
+DNA Pairing
+
+The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+
+
+function pairElement(str) {
+  let missingPair;
+  let DNAPairArray = [];
+  for (let i = 0; i < str.length; i++) {
+    switch (str.charAt(i)) {
+      case 'A':
+        missingPair = "T";
+        let ATArray = new Array();
+        ATArray.push(str.charAt(i), missingPair);
+        DNAPairArray.push(ATArray);
+        break;
+      case 'T':
+        missingPair = "A";
+        let TAArray = new Array();
+        TAArray.push(str.charAt(i), missingPair);
+        DNAPairArray.push(TAArray);
+        break;
+      case 'G':
+        missingPair = "C";
+        let GCArray = new Array();
+        GCArray.push(str.charAt(i), missingPair);
+        DNAPairArray.push(GCArray);
+        break;
+      case 'C':
+        missingPair = "G";
+        let CGArray = new Array();
+        CGArray.push(str.charAt(i), missingPair);
+        DNAPairArray.push(CGArray);
+        break;
+    }
+  }
+  return DNAPairArray;
+}
+pairElement("GCG");
+
+
+
+
+
+
+
+
+*/
