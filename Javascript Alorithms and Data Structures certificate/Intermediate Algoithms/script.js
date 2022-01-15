df2f60744f9e7510f14947b40ecd5ddb49220028
VagTsop <vatsop52@gmail.com>
2022-01-15 17:47:29 +0200

    add 'Sorted Union' Solution

--- Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
+++ Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
@@ -243,12 +243,30 @@ function pairElement(str) {
   return DNAPairArray;
 }
 pairElement("GCG");
+*/
+
+/*
+-Sorted Union
+
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
 
 
-
-
-
-
-
-
+function uniteUnique(arr) {
+  let newArr = [];
+  let flattedArray = [];
+  let finalUniqueArray = [];
+  for (let i = 0; i < arguments.length; i++) {
+    newArr = [...arguments];
+  }
+  flattedArray = newArr.flat();
+  finalUniqueArray = [...new Set(flattedArray)];
+  return finalUniqueArray;
+}
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 */
