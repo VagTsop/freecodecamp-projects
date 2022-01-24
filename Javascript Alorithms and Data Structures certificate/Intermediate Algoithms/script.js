e1e7dde9c5feb3b61b16b0017b1342177418b7f6
VagTsop <vatsop52@gmail.com>
2022-01-24 19:33:12 +0200

    add 'Drop it' solution

--- Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
+++ Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
@@ -404,5 +404,25 @@ function smallestCommons(arr) {
   return arr;
 }
 smallestCommons([1, 5]);
+*/
 
+/*
+
+- Drop it
+Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+function dropElements(arr, func) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      newArr = arr.slice(i);
+      break;
+    }
+  }
+  return newArr;
+}
+dropElements([1, 2, 3, 7, 4], function (n) {
+  return n > 3;
+});
 */
