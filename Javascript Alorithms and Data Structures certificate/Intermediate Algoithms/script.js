4535f69d9baeddfac141b8c1110f93734f8c847e
VagTsop <vatsop52@gmail.com>
2022-01-24 15:15:09 +0200

    add 'Smallest Common Multiple' solution

--- Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
+++ Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
@@ -372,3 +372,37 @@ function isPrime(num) {
 }
 sumPrimes(10);
 */
+
+/*
+Smallest Common Multiple
+
+Find the smallest common multiple of the provided parameters that can be evenly divided by both,
+as well as by all sequential numbers in the range between these parameters.
+The range will be an array of two numbers that will not necessarily be in numerical order.
+For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is
+ also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
+
+function smallestCommons(arr) {
+  const small = Math.min(...arr);
+  const big = Math.max(...arr);
+  let scm = big;
+  
+  while (true) {
+    let isScm = true;
+    for (let i = small; i <= big; i++) {
+      if (scm % i !== 0) {
+        isScm = false;
+        break;
+      }
+    }
+    if (isScm) {
+      return scm;
+    } else {
+      scm++;
+    }
+  }
+  return arr;
+}
+smallestCommons([1, 5]);
+
+*/
