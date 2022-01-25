0e2f279821ebaa7b52879cb4f75d39de50997209
VagTsop <vatsop52@gmail.com>
2022-01-25 15:32:41 +0200

    add 'Arguments Optional' exercise

--- Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
+++ Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
@@ -486,3 +486,28 @@ truthCheck(
   "age"
 );
 */
+/*
+- Arguments Optional
+Create a function that sums two arguments together. If only one argument is provided, then
+return a function that expects one argument and returns the sum.
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function
+
+
+function addTogether(a, b) {
+  for (let i = 0; i < arguments.length; i++) {
+    if (!Number.isFinite(arguments[i])) {
+      return undefined;
+    }
+  }
+  if (!arguments[1]) {
+    return function (b) {
+      if (Array.isArray(b)) {
+        return undefined;
+      }
+      return a + b;
+    };
+  }
+  return a + b;
+}
+addTogether(2)([3]);
+*/
