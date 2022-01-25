faaef392172a2bee3e92899c3d9a7865ef32d6c4
VagTsop <vatsop52@gmail.com>
2022-01-25 12:41:31 +0200

    add 'Everything Be True' Solution

--- Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
+++ Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
@@ -458,3 +458,31 @@ function binaryAgent(str) {
 binaryAgent(`01000001 01110010 01100101 01101110 00100111 01110100
  00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111`);
 */
+
+/*
+- Everything Be True
+
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+
+function truthCheck(collection, pre) {
+  for (let i = 0; i < collection.length; i++) {
+    if (!collection[i].hasOwnProperty(pre) || !collection[i][pre]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+truthCheck(
+  [
+    { user: "Tinky-Winky", sex: "male", age: 0 },
+    { user: "Dipsy", sex: "male", age: 3 },
+    { user: "Laa-Laa", sex: "female", age: 5 },
+    { user: "Po", sex: "female", age: 4 }
+  ],
+  "age"
+);
+*/
