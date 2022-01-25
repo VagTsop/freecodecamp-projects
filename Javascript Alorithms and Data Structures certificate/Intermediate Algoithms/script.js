41ab62f9ce228c199129fb3a38a6b70a9dd779dc
VagTsop <vatsop52@gmail.com>
2022-01-25 11:33:20 +0200

    Add 'Binary Agents' Solution

--- Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
+++ Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
@@ -440,3 +440,21 @@ function steamrollArray(arr) {
 }
 steamrollArray([1, [2], [3, [[4]]]]);
 */
+
+/*
+-Binary Agents
+
+Return an English translated sentence of the passed binary string.
+
+The binary string will be space separated.
+
+function binaryAgent(str) {
+
+  return str.split(' ') //Split string in array of binary chars
+    .map(bin => String.fromCharCode(parseInt(bin, 2))) //Map every binary char to real char
+    .join(''); //Join the array back to a string
+}
+
+binaryAgent(`01000001 01110010 01100101 01101110 00100111 01110100
+ 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111`);
+*/
