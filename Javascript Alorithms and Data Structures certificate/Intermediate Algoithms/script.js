0fa024fb92e543e70d258065c313bf30119b2606
VagTsop <vatsop52@gmail.com>
2022-01-26 17:00:26 +0200

    add description

--- Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
+++ Javascript Alorithms and Data Structures certificate/Intermediate Algoithms/script.js
@@ -573,6 +573,13 @@ function orbitalPeriod(arr) {
 */
 
 /*
+
+Palindrome Checker
+
+Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
 function palindrome(str) {
   // 1. remove all alphanumeric characters (punctuation, spaces and symbols) and turn
   // everything into the same case (lower or upper case) in order to check for palindromes.
